4f9809e20ea2a936d7e8e7cf2d56c6ef133d789e
daniel155158 <daniel155158@gmail.com>
2022-12-31 10:34:10 +0800

    feat: add login and register page

--- app.js
+++ app.js
@@ -12,8 +12,29 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 進入首頁
 app.get('/', (req, res) => {
   res.send('Hello world!')
+})
+// 進入登入頁
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 提交登入資訊
+app.post('/users/login', (req, res) => {
+  res.send('Login ok!')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('Logout ok!')
+})
+// 進入註冊頁
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 提交註冊資訊
+app.post('/users/register', (req, res) => {
+  res.send('Register ok!')
 })
 
 app.listen(PORT, () => {
