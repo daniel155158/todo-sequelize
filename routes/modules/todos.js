5f8b8a57c4587273ef5beabd95f2abd7ee32889a
daniel155158 <daniel155158@gmail.com>
2023-01-04 15:26:31 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,19 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// 進入新增頁面
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+// 新增一筆todo
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // 查詢todo
 router.get('/:id', (req, res) => {
   const id = req.params.id
