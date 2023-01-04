996819025ad77d1b42f52bb92547909ae3f284c4
daniel155158 <daniel155158@gmail.com>
2023-01-04 15:53:33 +0800

    feat: add edit todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -15,12 +15,33 @@ router.post('/', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-
 // 查詢todo
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+// 進入edit頁面
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+// 修改一筆todo
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.update({
+    name: name,
+    isDone: isDone === 'on'
+  }, {
+    where: {
+      id: id
+    }
+  })
+    .then(() => { res.redirect(`/todos/${id}`) })
     .catch(error => console.log(error))
 })
 
