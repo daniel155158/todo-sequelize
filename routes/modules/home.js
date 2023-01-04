c2dcd79194bcf674ae89c8a9ad0579294f94055f
daniel155158 <daniel155158@gmail.com>
2023-01-04 16:12:40 +0800

    fix: home page modify to only show UserId's todos

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,9 +6,13 @@ const Todo = db.Todo
 
 // 進入首頁
 router.get('/', (req, res) => {
+  const UserId = req.user.id
   return Todo.findAll({
     raw: true,
-    nest: true
+    nest: true,
+    where: {
+      UserId: UserId
+    }
   })
     .then((todos) => { return res.render('index', { todos: todos }) })
     .catch((error) => { return res.status(422).json(error) })
